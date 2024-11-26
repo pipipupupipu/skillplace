46aac5199ee67c1d6d31eb16ad475f0b9661e612
pipipupupipu <96349880+pipipupupipu@users.noreply.github.com>
2024-11-26 22:04:54 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,24 @@
+
+#1st program
+
+print(9**0.5*5)
+
+#2nd program
+
+print(9.99 > 9.98 and 1000 !=1000.1)
+
+#3rd program
+
+a = 2*2+2
+b = 2*(2+2)
+
+print(a)
+print(b)
+print(a==b)
+
+#4th program
+
+string = '123.456'
+number = float(string)
+integer_part = int(number)
+print(int((number-integer_part)*10))
